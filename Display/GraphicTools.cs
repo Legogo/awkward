0fad2ec4bfd7c7e9b23378553090cb0d24fb958c
André Berlemont <andre.berlemont@gmail.com>
2014-08-12 23:36:50 +0200

    added 1x1 planes pivot center and botleft, added GraphicTools

---
+++ Display/GraphicTools.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphicTools : MonoBehaviour {
+  
+  static public void setAlpha(Material m, float alpha){
+    if(m == null) return;
+    //if(m.color == null){ Debug.LogWarning(m.name);return; }
+    alpha = Mathf.Clamp(alpha,0f,1f);
+    Color col = m.color;
+    col.a = alpha;
+    m.color = col;
+
+    //Debug.Log(m.name+","+m.color.a);
+  }
+
+}
