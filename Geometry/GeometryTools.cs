356fb454e39816510796dbe85d3d156c44400fe7
André Berlemont <andre.berlemont@gmail.com>
2014-08-12 23:54:45 +0200

    fix GeometryPlane generator, added size to generator, added checker 32x32 texture

--- Geometry/GeometryTools.cs
+++ Geometry/GeometryTools.cs
@@ -3,20 +3,28 @@ using System.Collections;
 
 public class GeometryTools : MonoBehaviour {
 	
-	static public Mesh createPlane(string name, Vector3 a, Vector3 b, Vector3 c, Vector3 d){
+	static public Mesh createPlaneMesh(string name, Vector3 a, Vector3 b, Vector3 c, Vector3 d){
 		Mesh msh = new Mesh();
 		msh.name = name;
 		drawPlane(msh, a,b,c,d);
 		return msh;
 	}
 	
-	static public GameObject createPlaneObject(string name, Vector3 a, Vector3 b, Vector3 c, Vector3 d){
+	static public GameObject createPlaneGameObject(string name, Vector3 a, Vector3 b, Vector3 c, Vector3 d){
 		GameObject obj = new GameObject(name);
-		Mesh msh = createPlane("MSH_"+name,a,b,c,d);
+    Mesh msh = createPlaneMesh("MSH_"+name,a,b,c,d);
 		obj.AddComponent<MeshFilter>().mesh = msh;
 		obj.AddComponent<MeshRenderer>();
 		return obj;
 	}
+
+  static public GameObject getStandardPlaneCenter(string name, float size){
+    size *= 0.5f;
+    return GeometryTools.createPlaneGameObject(name, new Vector3(-size, -size, 0f), new Vector3(size, -size, 0f), new Vector3(size, size, 0f), new Vector3(-size, size, 0f));
+  }
+  static public GameObject getStandardPlane(string name, float size){
+    return GeometryTools.createPlaneGameObject(name, new Vector3(0f, 0f, 0f), new Vector3(size, 0f, 0f), new Vector3(size, size, 0f), new Vector3(0f, size, 0f));
+  }
 	
 	/* c d
 	 * |-|
@@ -26,8 +34,8 @@ public class GeometryTools : MonoBehaviour {
 	 * */
 	static public void drawPlane(Mesh msh, Vector3 a, Vector3 b, Vector3 c, Vector3 d){
 		//VERTICES (bottom, bottom, top, top)
-		Vector3[] vert = { a, b, c, d };
-		msh.vertices = vert;
+    Vector3[] vertx = {a, b, c, d};
+    msh.vertices = vertx;
 		msh.RecalculateNormals();
 		
 		//UVS
@@ -40,7 +48,7 @@ public class GeometryTools : MonoBehaviour {
 		msh.uv = uvs;
 		
 		//TRIANGLES
-		int[] tri = {0, 2, 1, 2, 3, 1};
+		int[] tri = {0,1,2,0,2,3};
 		msh.triangles = tri;
 		
 		msh.RecalculateNormals();
