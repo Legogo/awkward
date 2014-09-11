8f8da46d8cec79a466b1c56b2a14d8c076583f07
André Berlemont <andre.berlemont@gmail.com>
2014-09-11 16:57:21 +0200

    added cam screen bounds info script

---
+++ Camera/CameraScreenBounds.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Camera screen bounds.
+/// Solve bottom left / top right / center corner
+/// </summary>
+
+public class CameraScreenBounds : MonoBehaviour {
+  
+  public Vector3 bottomLeft;
+  public Vector3 topRight;
+  public Vector3 centerPoint;
+
+  Camera cam;
+
+  void Start(){
+    cam = Camera.main;
+    update();
+  }
+  
+  void update(){
+    //get camera z
+    Vector3 pos = Vector3.zero;
+    pos.z = cam.nearClipPlane;
+    
+    bottomLeft = cam.ScreenToWorldPoint(pos);
+    
+    pos.x = Screen.width;
+    pos.y = Screen.height;
+    
+    topRight = cam.ScreenToWorldPoint(pos);
+
+    centerPoint = bottomLeft + ((topRight - bottomLeft) * 0.5f);
+  }
+}
