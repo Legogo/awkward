b4b8084afbb2fc01503f872cc09dcd5b20c31fe7
André Berlemont <andre.berlemont@gmail.com>
2014-10-29 11:16:00 +0100

    added some editor tools

---
+++ Editor/Tools/ApplyPrefabRoutine.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class ApplyPrefabRoutine : EditorWindow {
+
+  [MenuItem ("Tools/ApplyPrefab #a")]
+  static void init(){
+    GameObject s = Selection.activeGameObject;
+    if(s == null) return;
+    apply(s);
+
+    if(s.transform.childCount > 0){
+      foreach(Transform t in s.transform){
+        apply(t.gameObject);
+      }
+    }
+  }
+
+  //http://forum.unity3d.com/threads/apply-prefab-script.140705/
+  static void apply(GameObject obj){
+    Object o = PrefabUtility.GetPrefabParent(obj);
+    Debug.Log(o);
+
+    if(o != null){
+      PrefabUtility.ReplacePrefab(obj, o, ReplacePrefabOptions.ConnectToPrefab | ReplacePrefabOptions.ReplaceNameBased);
+    }
+
+    Debug.Log("[TOOLS] applied prefab "+obj.name);
+  }
+}
---
+++ Editor/Tools/RoutineSortObjects.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections.Generic;
+
+public class RoutineSortObjects : EditorWindow {
+  
+  [MenuItem ("Tools/Nodes tools")]
+  static void init(){
+    EditorWindow.GetWindow(typeof(ApplyPrefabRoutine));
+  }
+  
+  void OnEnabled(){
+    // called on window creation
+  }
+  
+  void OnGUI(){
+    /*
+    GUILayout.Label("label");
+    EditorGUILayout.LabelField("label left", "label right");
+    EditorGUILayout.Separator();
+    EditorGUILayout.ObjectField("Title", objectHandle, typeof(objectClassName), true);
+    */
+    if (GUILayout.Button("Re-order nodes")){
+      reorder();
+    }
+  }
+  
+  void reorder(){
+    GameObject select = Selection.activeGameObject;
+    if(select == null)  return;
+    
+    if(select.name.IndexOf("_path") > -1){
+      List<Transform> children = new List<Transform>();
+      
+      for (int i = 1; i < 50; i++) {
+        foreach(Transform c in select.transform){
+          if(c.name.IndexOf("_"+i) > -1){
+            children.Add(c);
+          }
+        }
+      }
+      
+      for (int i = 0; i < children.Count; i++) {
+        children[i].transform.parent = null;
+        children[i].transform.parent = select.transform;
+      }
+      //Debug.Log(children.Count);
+      //children.ToArray().orOrder();
+    }
+  }
+}
