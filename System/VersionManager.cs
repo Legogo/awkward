011bc7e7da34f0c17b790ec416a7b2f2b7b26bea
Andre Berlemont <andre.berlemont@gmail.com>
2014-12-04 13:38:17 +0100

    clean versionmanager

--- System/VersionManager.cs
+++ System/VersionManager.cs
@@ -1,14 +1,11 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(TextMesh))]
 public class VersionManager : MonoBehaviour {
 	
-  string website = ""; // when you click on version this website open
-
-  public Font font;
-  public float fontSize = 1f;
-
   public string version = "";
+  Renderer render;
 	TextMesh txt;
 
   float waitOnScreenTime = 2f;
@@ -17,20 +14,12 @@ public class VersionManager : MonoBehaviour {
   void Awake(){
     DontDestroyOnLoad(gameObject);
     manager = this;
-
   }
 
   void Start(){
-    StartCoroutine(createMesh());
-  }
-  
-  IEnumerator createMesh(){
-    txt = gameObject.AddComponent<TextMesh>();
-    yield return new WaitForSeconds(0.1f);
-    txt.font = font;
-    txt.characterSize = fontSize;
+    txt = gameObject.GetComponent<TextMesh>();
+    render = txt.renderer;
     updateText();
-    yield return new WaitForSeconds(0.1f);
     startFading();
   }
 
@@ -45,23 +34,23 @@ public class VersionManager : MonoBehaviour {
   IEnumerator fadeProcess(){
     setAlpha(0f);
 
-    while(renderer.sharedMaterial.color.a < 1f){
-      setAlpha(renderer.sharedMaterial.color.a + (Time.deltaTime * fadeSpeed));
+    while(render.sharedMaterial.color.a < 1f){
+      setAlpha(render.sharedMaterial.color.a + (Time.deltaTime * fadeSpeed));
       yield return null;
     }
 
     yield return new WaitForSeconds(waitOnScreenTime);
 
-    while(renderer.sharedMaterial.color.a > 0f){
-      setAlpha(renderer.sharedMaterial.color.a - (Time.deltaTime * fadeSpeed));
+    while(render.sharedMaterial.color.a > 0f){
+      setAlpha(render.sharedMaterial.color.a - (Time.deltaTime * fadeSpeed));
       yield return null;
     }
   }
 
   void setAlpha(float alpha){
-    Color col = renderer.sharedMaterial.color;
+    Color col = render.sharedMaterial.color;
     col.a = alpha;
-    renderer.sharedMaterial.color = col;
+    render.sharedMaterial.color = col;
   }
 
   int convertVersion(string v){
@@ -84,28 +73,8 @@ public class VersionManager : MonoBehaviour {
 		if(Input.GetKeyUp(KeyCode.Equals)){
       toggleVersion();
 		}
-
-    checkClick();
 	}
 
-  void checkClick(){
-    if(txt.renderer.material.color.a <= 0f) return;
 
-    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-    RaycastHit hit;
-    if (Physics.Raycast(ray, out hit, 100)){
-      //Debug.DrawLine(ray.origin, hit.point);
-      if(Input.GetMouseButtonUp(0)){
-        goGrabLastVersion();
-      }
-    }
-
-  }
-
-  void goGrabLastVersion(){
-    Debug.Log("opening browser to grab last version");
-    if(website.Length > 1) Application.OpenURL (website);
-  }
-	
   static public VersionManager manager;
 }
